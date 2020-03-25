cdd9fa722ce754ff90838be3c9ac2d3fa5303428
sonish <sonishmzn95@gmail.com>
2020-03-25 22:36:25 +0545

    hook, changing state in functional component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,59 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const App = props => {
+  // use state returns array with two element current state and function that allows to update state
+  const [personState, setPersonState] = useState({
     persons: [
       { name: "Max", age: 28 },
       { name: "Manu", age: 34 },
       { name: "sumuki", age: 34 }
     ],
     otherstate: "some other state"
-  };
+  });
+  // you can use two useState so that changing one state does not impact other
+  const [otherSeperateState, setOtherState] = useState("other seperate value");
 
-  switchNameHandler = () => {
+  // console.log(personState, otherSeperateState);
+  // debugger;
+  const switchNameHandler = () => {
     // console.log("hello wor");
 
-    this.setState({
+    setPersonState({
       persons: [
         { name: "sonish", age: 34 },
         { name: "Manu", age: 34 },
-        { name: "sumuki", age: 34 }
+        { name: "sumuki", age: 3 }
       ]
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello react app</h1>
-        <p>Hello world</p>
-        <button onClick={this.switchNameHandler}> Switch Name </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+  return (
+    <div className="App">
+      <h1>Hello react app</h1>
+      <p>Hello world</p>
+      <button onClick={switchNameHandler}> Switch Name </button>
+      <Person
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
+      />
+      <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+      />
+      <Person
+        name={personState.persons[2].name}
+        age={personState.persons[2].age}
+      />
 
-        <Person name="momo" age="10">
-          This is inside person or ith is achildren hobby is playing
-        </Person>
-      </div>
-    );
-  }
+      <Person name="momo" age="10">
+        This is inside person or ith is achildren hobby is playing
+      </Person>
+      <p>the other state-> {personState.otherstate}</p>
+    </div>
+  );
+
   // return React.createElement(
   //   "div",
   //   null,
@@ -59,6 +64,6 @@ class App extends Component {
   //   ),
   //   "hey im a react app"
   // );
-}
+};
 
 export default App;
